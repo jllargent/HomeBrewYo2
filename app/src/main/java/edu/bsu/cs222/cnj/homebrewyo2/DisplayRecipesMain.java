5098e85d50ee729f2b8433335ba2eb87fa0f9281
Jon Largent <jllargent@bsu.edu>
2016-03-10 02:39:15 -0500

    add style and name buttons to the recipe page. both buttons go to their pages

--- app/src/main/java/edu/bsu/cs222/cnj/homebrewyo2/DisplayRecipesMain.java
+++ app/src/main/java/edu/bsu/cs222/cnj/homebrewyo2/DisplayRecipesMain.java
@@ -3,6 +3,7 @@ package edu.bsu.cs222.cnj.homebrewyo2;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class DisplayRecipesMain extends AppCompatActivity {
 
@@ -13,4 +14,15 @@ public class DisplayRecipesMain extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_recipes_main);
     }
+
+    public void goStyleRecipes(View view){
+        Intent recipeStyleIntent = new Intent(this, RecipesStylesActivity.class);
+        startActivity(recipeStyleIntent);
+    }
+
+    public void goNameRecipes(View view){
+        Intent recipeNameIntent = new Intent(this, NameRecipeActivity.class);
+        startActivity(recipeNameIntent);
+    }
+
 }
