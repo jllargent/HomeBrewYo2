8e73011a10f917344061493d669fac42f2782dce
Cody LeFan-Weaver <crlefanweaver@gmail.com>
2016-03-10 14:48:13 -0500

    Made adjustments to the timers.

--- app/src/main/java/edu/bsu/cs222/cnj/homebrewyo2/TimerActivity.java
+++ app/src/main/java/edu/bsu/cs222/cnj/homebrewyo2/TimerActivity.java
@@ -3,14 +3,24 @@ package edu.bsu.cs222.cnj.homebrewyo2;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextClock;
+import android.widget.TextView;
 
 public class TimerActivity extends AppCompatActivity {
 
     Intent intent = getIntent();
+    Button btnStart, btnStop;
+    TextView textViewTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timer);
+
+        btnStart = (Button) findViewById(R.id.btnStart);
+        btnStop = (Button) findViewById(R.id.btnStop);
+        textViewTime = (TextView) findViewById(R.id.timer1);
+
     }
 }
