47561f2654f86fd22c48f7c80fb4e539dca49525
Jon Largent <jllargent@bsu.edu>
2016-03-14 11:44:14 -0400

    resolved error

--- app/src/main/java/edu/bsu/cs222/cnj/homebrewyo2/AmberAles.java
+++ app/src/main/java/edu/bsu/cs222/cnj/homebrewyo2/AmberAles.java
@@ -1,8 +1,8 @@
 package edu.bsu.cs222.cnj.homebrewyo2;
 
 import android.content.Intent;
-import android.support.v7.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 public class AmberAles extends AppCompatActivity {
