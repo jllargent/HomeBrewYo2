66c9db8ba09433e6e97b4992125ada24d94d56db
Cody LeFan-Weaver <crlefanweave@bsu.edu>
2016-03-17 10:45:30 -0400

    Got through to Physics, so far.

--- app/src/main/java/edu/bsu/cs222/cnj/homebrewyo2/Hoprocker.java
+++ app/src/main/java/edu/bsu/cs222/cnj/homebrewyo2/Hoprocker.java
@@ -4,13 +4,24 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class Hoprocker extends AppCompatActivity {
+
+    private Button timeSet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hoprocker);
+
+        timeSet = (Button) findViewById(R.id.hopRockTimer);
+        timeSet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TimerActivity.setTime(75 * 60);
+            }
+        });
     }
     public void goHopRockerIngredients(View view){
         Intent onClick = new Intent(this, HoprockerIngredients.class);
--- app/src/main/java/edu/bsu/cs222/cnj/homebrewyo2/Physics.java
+++ app/src/main/java/edu/bsu/cs222/cnj/homebrewyo2/Physics.java
@@ -4,13 +4,24 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class Physics extends AppCompatActivity {
+
+    Button timeSet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_physics);
+
+        timeSet = (Button) findViewById(R.id.physTimer);
+        timeSet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TimerActivity.setTime(75 * 60);
+            }
+        });
     }
     public void goPhysicsIngredients(View view){
         Intent onClick = new Intent(this, PhysicsIngredients.class);
