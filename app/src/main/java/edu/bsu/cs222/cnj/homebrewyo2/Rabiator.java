b35ed8538aa10cfe7fb1307e49192a35e4e521a1
Cody LeFan-Weaver <crlefanweave@bsu.edu>
2016-03-17 10:57:11 -0400

    All activities tied to timer. Recommended: button to jump to timer page.

--- app/src/main/java/edu/bsu/cs222/cnj/homebrewyo2/Rabiator.java
+++ app/src/main/java/edu/bsu/cs222/cnj/homebrewyo2/Rabiator.java
@@ -4,13 +4,24 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class Rabiator extends AppCompatActivity {
+
+    Button timeSet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rabiator);
+
+        timeSet = (Button) findViewById(R.id.rabTimer);
+        timeSet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TimerActivity.setTime(60 * 60);
+            }
+        });
     }
     public void goRabiatorIngredients(View view){
         Intent onClick = new Intent(this, RabiatorIngredients.class);
--- app/src/main/java/edu/bsu/cs222/cnj/homebrewyo2/Riptide.java
+++ app/src/main/java/edu/bsu/cs222/cnj/homebrewyo2/Riptide.java
@@ -4,13 +4,24 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class Riptide extends AppCompatActivity {
+
+    Button timeSet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_riptide);
+
+        timeSet = (Button) findViewById(R.id.ripTimer);
+        timeSet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TimerActivity.setTime(75 * 60);
+            }
+        });
     }
     public void goRiptideIngredients(View view){
         Intent onClick = new Intent(this, RiptideIngredients.class);
