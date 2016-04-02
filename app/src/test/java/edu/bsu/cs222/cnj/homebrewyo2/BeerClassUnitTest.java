c6a755814ad03070926b8df3fb28bb9cf9cacfc8
Nathan Peterson <napeterson@bsu.edu>
2016-04-02 18:59:20 -0400

    Create Beer Class Unit Test

---
+++ app/src/test/java/edu/bsu/cs222/cnj/homebrewyo2/BeerClassUnitTest.java
@@ -0,0 +1,21 @@
+package edu.bsu.cs222.cnj.homebrewyo2;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BeerClassUnitTest {
+    Beer testBeer = new Beer("Test Beer");
+    String description = "This is a description to test.";
+
+    @Before
+    public void createTestBeerElements(){
+        testBeer.setStyleOfBeer("TestingBeerType");
+        testBeer.setDescriptionOfBeer(description);
+        testBeer.setValueOfABV(5);
+    }
+
+
+
+}
