f6c6bce572728fc5dd885a006ba6fd2d82836631
Cody LeFan-Weaver <crlefanweave@bsu.edu>
2016-04-02 09:58:05 -0400

    Some unit testing I'm doing. Look at it, do some yourselves.

---
+++ app/src/androidTest/java/edu/bsu/cs222/cnj/homebrewyo2/UnitTesting.java
@@ -0,0 +1,37 @@
+package edu.bsu.cs222.cnj.homebrewyo2;
+
+import android.media.Image;
+import android.test.ActivityInstrumentationTestCase2;
+import com.robotium.solo.Solo;
+import android.test.suitebuilder.annotation.LargeTest;
+import android.widget.Button;
+import android.widget.ImageView;
+
+/**
+ * Created by Cody on 4/2/2016.
+ */
+public class UnitTesting extends ActivityInstrumentationTestCase2<MainActivity>{
+    private Solo solo;
+
+    public UnitTesting(){
+        super(MainActivity.class);
+    }
+
+    @Override
+    public void setUp() throws Exception{
+        super.setUp();
+        solo = new Solo(getInstrumentation(), getActivity());
+    }
+
+    @Override
+    public void tearDown() throws Exception{
+        solo.finishOpenedActivities();
+        super.tearDown();
+    }
+
+    public void test_MainChangeActivityToTimerOnButtonPress() throws Exception{
+        ImageView btn_timer = (ImageView) solo.getView(R.id.timerButton);
+        solo.clickOnView(btn_timer);
+        solo.assertCurrentActivity("check for activity", TimerActivity.class);
+    }
+}
