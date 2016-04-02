fc9592d31fdc00cca9cbe5be1534cebc24802481
Cody LeFan-Weaver <crlefanweave@bsu.edu>
2016-04-02 14:24:27 -0400

    Parser 66% done. Moved beerrecipes.xml to raw folder.

--- app/src/main/java/edu/bsu/cs222/cnj/homebrewyo2/MainActivity.java
+++ app/src/main/java/edu/bsu/cs222/cnj/homebrewyo2/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new XMLFileProcessor(this);
     }
 
     public void goRecipes(View view){
