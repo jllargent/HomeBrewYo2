ea0942009babfa447ff7eb4358c2c6cba3bd1e69
Nathan Peterson <napeterson@bsu.edu>
2016-04-03 01:32:23 -0400

    Fix BeerClass Unit Test for Title

--- app/src/test/java/edu/bsu/cs222/cnj/homebrewyo2/BeerClassUnitTest.java
+++ app/src/test/java/edu/bsu/cs222/cnj/homebrewyo2/BeerClassUnitTest.java
@@ -6,11 +6,12 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class BeerClassUnitTest {
-    Beer testBeer = new Beer("Test Beer");
+    Beer testBeer = new Beer();
     String description = "This is a description to test.";
 
     @Before
     public void createTestBeerElements(){
+        testBeer.setTitleOfBeer("Test Beer");
         testBeer.setStyleOfBeer("TestingBeerType");
         testBeer.setDescriptionOfBeer(description);
         testBeer.setValueOfABV(5);
