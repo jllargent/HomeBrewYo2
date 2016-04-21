2c75981cae9cb54d16db9e6391e3162a655023e0
Nathan Peterson <napeterson@bsu.edu>
2016-04-21 17:45:09 -0400

    Create builder factory in parser so test passes.

--- app/src/main/java/edu/bsu/cs222/cnj/homebrewyo2/NewXmlParser.java
+++ app/src/main/java/edu/bsu/cs222/cnj/homebrewyo2/NewXmlParser.java
@@ -1,17 +1,26 @@
 package edu.bsu.cs222.cnj.homebrewyo2;
 
 import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
 
+import java.io.IOException;
 import java.io.InputStream;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
 public class NewXmlParser {
 
     public InputStream xmlData;
     public Document document;
 
-    public NewXmlParser(InputStream input){
+    public NewXmlParser(InputStream input) throws IOException, SAXException, ParserConfigurationException {
         this.xmlData = input;
-
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        document = builder.parse(xmlData);
+        document.getElementsByTagName("recipe");
     }
 
 }
