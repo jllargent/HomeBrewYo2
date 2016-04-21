b0423768bb42a784e9aaf7ed6b0798d12e3fad6a
Nathan Peterson <napeterson@bsu.edu>
2016-04-21 17:53:12 -0400

    Refactor builder factory in parser -Put in its own method

--- app/src/main/java/edu/bsu/cs222/cnj/homebrewyo2/NewXmlParser.java
+++ app/src/main/java/edu/bsu/cs222/cnj/homebrewyo2/NewXmlParser.java
@@ -1,6 +1,7 @@
 package edu.bsu.cs222.cnj.homebrewyo2;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 import java.io.IOException;
@@ -14,9 +15,14 @@ public class NewXmlParser {
 
     public InputStream xmlData;
     public Document document;
+    public NodeList recipeNodeList;
 
     public NewXmlParser(InputStream input) throws IOException, SAXException, ParserConfigurationException {
         this.xmlData = input;
+        createXMLParser();
+    }
+
+    private void createXMLParser()throws IOException, SAXException, ParserConfigurationException {
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         DocumentBuilder builder = factory.newDocumentBuilder();
         document = builder.parse(xmlData);
