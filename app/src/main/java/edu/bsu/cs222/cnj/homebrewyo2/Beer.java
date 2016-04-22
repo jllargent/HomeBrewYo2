7d2f954e8530c5cda33b768c4be0d2f853fec89e
Cody LeFan-Weaver <crlefanweave@bsu.edu>
2016-04-22 13:02:44 -0400

    Make Lists for Malt and Hop objects in Beer class.

--- app/src/main/java/edu/bsu/cs222/cnj/homebrewyo2/Beer.java
+++ app/src/main/java/edu/bsu/cs222/cnj/homebrewyo2/Beer.java
@@ -2,11 +2,15 @@ package edu.bsu.cs222.cnj.homebrewyo2;
 
 import org.w3c.dom.Node;
 
+import java.util.List;
+
 /**
  * Created by Nathan on 4/21/2016.
  */
 public class Beer {
     private String name;
+    private List<Malt> malts;
+    private List<Hop> hops;
 
 
     public Beer(String nameFromFile){
