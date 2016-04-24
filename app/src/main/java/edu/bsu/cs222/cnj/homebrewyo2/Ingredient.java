3178552f52a32e0848a3797c24a76e3b29305cd0
Cody LeFan-Weaver <crlefanweaver@bsu.edu>
2016-04-24 19:28:38 -0400

    Make test for ingredient builder class, refactor code to have new ingredient and subclasses: malt, hop.

---
+++ app/src/main/java/edu/bsu/cs222/cnj/homebrewyo2/Ingredient.java
@@ -0,0 +1,18 @@
+package edu.bsu.cs222.cnj.homebrewyo2;
+
+import java.util.ArrayList;
+
+public class Ingredient {
+
+    protected String name;
+    protected String amount;
+
+    protected void setYeastName(String name){
+        this.name = name;
+    }
+
+    public String getYeastName(){
+        return this.name;
+    }
+}
+
