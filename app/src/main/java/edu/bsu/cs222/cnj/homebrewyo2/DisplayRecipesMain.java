4e1a90f876a3fbd7ad30c4808a33253e14a341e1
Nathan Peterson <napeterson@bsu.edu>
2016-04-24 21:11:59 -0400

    Pass Bundle Through activities

--- app/src/main/java/edu/bsu/cs222/cnj/homebrewyo2/DisplayRecipesMain.java
+++ app/src/main/java/edu/bsu/cs222/cnj/homebrewyo2/DisplayRecipesMain.java
@@ -5,18 +5,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import java.io.Serializable;
+import java.util.List;
+
 public class DisplayRecipesMain extends AppCompatActivity {
 
     Intent intent = getIntent();
+    List<Beer> recipeList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_recipes_main);
+        Bundle bundle = getIntent().getExtras();
+        recipeList = (List<Beer>) bundle.getSerializable("Recipe List");
     }
 
     public void goNameRecipes(View view){
         Intent recipeNameIntent = new Intent(this, NameRecipeActivity.class);
+        Bundle bundle = new Bundle();
+        bundle.putSerializable("RecipeList", (Serializable) recipeList);
+        recipeNameIntent.putExtras(bundle);
         startActivity(recipeNameIntent);
     }
 
