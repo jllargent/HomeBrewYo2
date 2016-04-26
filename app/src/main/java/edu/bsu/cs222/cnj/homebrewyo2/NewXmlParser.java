8966f6fc5482461465b02309dd605cc3e82be506
Nathan Peterson <napeterson@bsu.edu>
2016-04-26 15:56:45 -0400

    Refactored DRY violation in Parser -3 methods instead of 10

--- app/src/main/java/edu/bsu/cs222/cnj/homebrewyo2/NewXmlParser.java
+++ app/src/main/java/edu/bsu/cs222/cnj/homebrewyo2/NewXmlParser.java
@@ -21,82 +21,52 @@ public class NewXmlParser{
 
     public InputStream xmlData;
     public Document document;
-    NodeList recipeNodeList;
-    public NodeList recipeChildNodeList;
-    Element eElement;
-    String name;
+    private NodeList recipeNodeList;
     private BeerBuilder beerBuilder = new RecipeBuilder();
     private Beer currentBeer;
     public List<Beer> beerList = new ArrayList();
+    private Element currentElement;
 
     public NewXmlParser(InputStream input) throws IOException, SAXException, ParserConfigurationException {
         this.xmlData = input;
         createXMLParser();
         for(int temp = 0; temp < recipeNodeList.getLength(); temp++){
             Node currentNode = recipeNodeList.item(temp);
-            beerBuilder.createBeer();
             if(isFirstNode(currentNode)){
                 continue;
             }
-            findCurrentName((Element) currentNode);
-            findCurrentDescription((Element) currentNode);
-            findCurrentBuildTime((Element) currentNode);
-            findCurrentTemp((Element) currentNode);
-            findCurrentStyle((Element) currentNode);
-            findCurrentFermentTemp((Element) currentNode);
-            findCurrentABVPercent((Element) currentNode);
-            findCurrentIBUValue((Element) currentNode);
-            findCurrentTargetFinalGravity((Element) currentNode);
-            findCurrentTargetOriginalGravity((Element) currentNode);
+            currentElement = (Element) currentNode;
+
+            beerBuilder.createBeer();
+            beerBuilder.buildName(getElementStringValueByTag("name"));
+            beerBuilder.buildDescription(getElementStringValueByTag("description"));
+            beerBuilder.buildTime(getElementIntValueByTag("time"));
+            beerBuilder.buildTemp(getElementIntValueByTag("temp"));
+            beerBuilder.buildStyle(getElementStringValueByTag("style"));
+            beerBuilder.buildFermentTemp(getElementIntValueByTag("fermtemp"));
+            beerBuilder.buildABVPercent(getElementDoubleValueByTag("abvpercent"));
+            beerBuilder.buildIBUValue(getElementIntValueByTag("ibu"));
+            beerBuilder.buildTargetOriginalGravity(getElementDoubleValueByTag("targetog"));
+            beerBuilder.buildTargetFinalGravity(getElementDoubleValueByTag("targetfg"));
             currentBeer = beerBuilder.getBeer();
             beerList.add(currentBeer);
         }
     }
 
-    private void findCurrentName(Element currentElement) {
-        beerBuilder.buildName(currentElement.getElementsByTagName("name").item(0).getTextContent());
+    private String getElementStringValueByTag(String tag){
+         return (currentElement).getElementsByTagName(tag).item(0).getTextContent();
     }
-    private void findCurrentDescription(Element currentElement) {
-        beerBuilder.buildDescription(currentElement.getElementsByTagName("description").item(0).getTextContent());
+    private int getElementIntValueByTag(String tag){
+        return Integer.parseInt(currentElement.getElementsByTagName(tag).item(0).getTextContent());
     }
-    
-    private void findCurrentBuildTime(Element currentElement){
-        beerBuilder.buildTime(Integer.parseInt(currentElement.getElementsByTagName("time").item(0).getTextContent()));
-    }
-
-    private void findCurrentTemp(Element currentElement){
-        beerBuilder.buildTemp(Integer.parseInt(currentElement.getElementsByTagName("temp").item(0).getTextContent()));
-    }
-
-    private void findCurrentStyle(Element currentElement){
-        beerBuilder.buildStyle(currentElement.getElementsByTagName("style").item(0).getTextContent());
-    }
-
-    private void findCurrentFermentTemp(Element currentElement){
-        beerBuilder.buildFermentTemp(Integer.parseInt(currentElement.getElementsByTagName("fermtemp").item(0).getTextContent()));
-    }
-
-    private void findCurrentABVPercent(Element currentElement){
-        beerBuilder.buildABVPercent(Double.parseDouble(currentElement.getElementsByTagName("abvpercent").item(0).getTextContent()));
-    }
-
-    private void findCurrentIBUValue(Element currentElement){
-        beerBuilder.buildIBUValue(Integer.parseInt(currentElement.getElementsByTagName("ibu").item(0).getTextContent()));
-    }
-
-    private void findCurrentTargetFinalGravity(Element currentElement){
-        beerBuilder.buildTargetFinalGravity(Double.parseDouble(currentElement.getElementsByTagName("targetfg").item(0).getTextContent()));
-    }
-
-    private void findCurrentTargetOriginalGravity(Element currentElement){
-        beerBuilder.buildTargetOriginalGravity(Double.parseDouble(currentElement.getElementsByTagName("targetog").item(0).getTextContent()));
+    private double getElementDoubleValueByTag(String tag){
+        return Double.parseDouble(currentElement.getElementsByTagName(tag).item(0).getTextContent());
     }
 
     private void createXMLParser()throws IOException, SAXException, ParserConfigurationException {
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         DocumentBuilder builder = factory.newDocumentBuilder();
         document = builder.parse(xmlData);
-        //document.getElementsByTagName(RECIPE_TAG);
         document.getDocumentElement().normalize();
         recipeNodeList = document.getElementsByTagName(RECIPE_TAG);
     }
